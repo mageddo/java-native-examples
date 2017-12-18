f7c6aa929c2f7a4a42d51141057f367c76f102fe
Elvis de Freitas Souza <edigitalb@gmail.com>
2017-12-18 11:46:19 -0200

    structuring

--- src/main/java/nativeapi/jna/windows/cursortype/Main.java
+++ src/main/java/nativeapi/jna/windows/cursortype/Main.java
@@ -1,5 +1,6 @@
 package nativeapi.jna.windows.cursortype;
 
+import com.sun.jna.Memory;
 import com.sun.jna.Native;
 import com.sun.jna.Structure;
 import com.sun.jna.platform.win32.*;
@@ -20,38 +21,36 @@ import java.util.List;
  *
  * @deprecated - it is incomplete, this sample is not working yet, see src/main/c/cmd/windows/cursortype.c
  */
-@Deprecated
 public class Main {
 
 	public interface User32 extends com.sun.jna.Library {
 		User32 INSTANCE = com.sun.jna.Native.loadLibrary("User32.dll", User32.class);
 
-
-//		BOOL WINAPI GetCursorPos(
-//			_Out_ LPPOINT lpPoint
+		//		BOOL WINAPI GetCursorInfo(
+//			_Inout_ PCURSORINFO pci
 //		);
-
-		boolean GetCursorPos(LPoint p);
+		int GetCursorInfo(CURSORINFO cursorinfo);
 
 		com.sun.jna.platform.win32.WinDef.HCURSOR GetCursor();
-//		BOOL WINAPI GetIconInfo(
+
+		//		BOOL WINAPI GetIconInfo(
 //			_In_  HICON     hIcon,
 //			_Out_ PICONINFO piconinfo
 //		);
 		boolean GetIconInfo(WinDef.HICON hicon, ICONINFO iconinfo);
 
 
-//		LPTSTR MAKEINTRESOURCE(
+		//		LPTSTR MAKEINTRESOURCE(
 //			WORD wInteger
 //		);
-			String MAKEINTRESOURCE(WinDef.WORD code);
+		String MAKEINTRESOURCE(WinDef.WORD code);
 
 //		HICON WINAPI LoadIcon(
 //			_In_opt_ HINSTANCE hInstance,
 //			_In_     LPCTSTR   lpIconName
 //		);
 
-		WinDef.HICON LoadIcon(WinDef.HINSTANCE hInstance, String   lpIconName );
+		WinDef.HICON LoadIcon(WinDef.HINSTANCE hInstance, String lpIconName);
 
 //		HANDLE WINAPI LoadImage(
 //			_In_opt_ HINSTANCE hinst,
@@ -64,69 +63,64 @@ public class Main {
 
 		WinNT.HANDLE LoadImage(
 			WinDef.HINSTANCE hinst,
-			String   lpszName,
-			int      uType,
-			int       cxDesired,
-			int       cyDesired,
-			int      fuLoad
+			String lpszName,
+			int uType,
+			int cxDesired,
+			int cyDesired,
+			int fuLoad
 		);
 	}
 
 	public static void main(String[] args) throws InterruptedException, IOException {
+		final CURSORINFO cursorinfo = new CURSORINFO();
+		System.out.printf("success=%b, info=%s, error=%d", User32.INSTANCE.GetCursorInfo(cursorinfo), cursorinfo, Native.getLastError());
+		Thread.sleep(1000);
+	}
 
-//		URL url = Main.class.getResource("/numbers.txt");
-//		while(true){
-//			URLConnection conn = url.openConnection();
-//			System.out.println(conn.getLastModified());
-//		String msg = "IDC_ARROW";
-//		Memory m = new Memory(msg.length() + 1 );
-//		m.setString(0, msg);
-//m.getPointer(0)
-//		WinDef.HICON icon = new WinDef.HICON(new WinNT.HANDLE());
-//		WinDef.HICON icon = new WinDef.HICON(new Pointer(new WinDef.WORD(32650).longValue()));
-//		System.out.println(icon);;
-//			ICONINFO inf = new ICONINFO();
-//			System.out.println(User32.INSTANCE.GetIconInfo(icon, inf));
-//		System.out.println(User32.INSTANCE.MAKEINTRESOURCE(new WinDef.WORD(32650)));
-//		System.out.println(com.sun.jna.platform.win32.User32.INSTANCE.LoadImage(null, "OCR_APPSTARTING", 2, 0, 0, WinUser.LR_MONOCHROME));
-//		System.out.println(Native.getLastError());
-//		System.out.println(User32.INSTANCE.LoadImage(null, "OCR_IDI_APPLICATION", 2, 0, 0, 0));
-//	System.exit(-1);
-		while (true){
-//			ICONINFO inf = new ICONINFO();
-//			System.out.println(User32.INSTANCE.GetCursor().getPointer().);
-//			System.out.println(User32.INSTANCE.GetIconInfo(User32.INSTANCE.GetCursor(), inf));
-//			System.out.println(inf.hbmMask);
-//			System.out.println(inf.xHotspot);
-//			LPoint p = new LPoint();
-//			System.out.println(User32.INSTANCE.GetCursorPos(p));
-//			System.out.println(p);
-			System.out.println(User32.INSTANCE.GetCursor());
-			System.out.println(Native.getLastError());
-			Thread.sleep(1000);
+
+	public static class LPoint extends Structure {
+
+		public long x, y;
+
+		@Override
+		protected List<String> getFieldOrder() {
+			return Arrays.asList("x", "y");
+		}
+	}
+
+	//	typedef struct {
+//		DWORD   cbSize;
+//		DWORD   flags;
+//		HCURSOR hCursor;
+//		POINT   ptScreenPos;
+//	} CURSORINFO, *PCURSORINFO, *LPCURSORINFO;
+	public static class CURSORINFO extends Structure {
+
+		public int cbSize;
+		public int flags;
+		public WinDef.HCURSOR hCursor;
+		public WinDef.POINT ptScreenPos;
+
+		public CURSORINFO() {
+			this.cbSize = Native.getNativeSize(CURSORINFO.class, null);
+		}
+
+		@Override
+		protected List<String> getFieldOrder() {
+			return Arrays.asList("cbSize", "flags", "hCursor", "ptScreenPos");
 		}
 	}
 
 	public static class ICONINFO extends Structure {
-		public boolean    fIcon;
-		public WinDef.DWORD   xHotspot;
-		public WinDef.DWORD   yHotspot;
+		public boolean fIcon;
+		public WinDef.DWORD xHotspot;
+		public WinDef.DWORD yHotspot;
 		public WinDef.HBITMAP hbmMask;
 		public WinDef.HBITMAP hbmColor;
 
 		@Override
 		protected List<String> getFieldOrder() {
 			return Arrays.asList("fIcon", "xHotspot", "yHotspot", "hbmMask", "hbmColor");
-		}
-	}
-
-	public static class LPoint extends Structure {
-
-		public long x,y;
-
-		@Override
-		protected List<String> getFieldOrder() {
-			return Arrays.asList("x", "y");
 		}
 	}
 }
