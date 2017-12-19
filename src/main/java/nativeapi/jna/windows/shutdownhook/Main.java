e8e000c3e699550f0da252db994606713b93fb01
Elvis de Freitas Souza <edigitalb@gmail.com>
2017-12-19 14:35:55 -0200

    documenting

--- src/main/java/nativeapi/jna/windows/shutdownhook/Main.java
+++ src/main/java/nativeapi/jna/windows/shutdownhook/Main.java
@@ -1,14 +1,14 @@
 package nativeapi.jna.windows.shutdownhook;
 
 /**
- * https://msdn.microsoft.com/en-us/library/windows/desktop/ms633570(v=vs.85).aspx
+ * WinProc creating sample in c++ - https://msdn.microsoft.com/en-us/library/windows/desktop/ms633570(v=vs.85).aspx
  * https://stackoverflow.com/questions/21369256/how-to-use-wndproc-as-a-class-function
  * https://stackoverflow.com/questions/47837851/javafx-how-to-detect-windows-logoff-shutdown-request
  * https://msdn.microsoft.com/en-us/library/windows/desktop/aa376889.aspx
  * creating a winproc to handle windows events - https://msdn.microsoft.com/en-us/library/windows/desktop/aa376890.aspx
  * 	how to create a winproc at JNA - https://stackoverflow.com/questions/4678247/java-jna-windowproc-implementation
  * criando uma window - https://msdn.microsoft.com/library/windows/desktop/ms632680
- * criando uma windows class - https://msdn.microsoft.com/pt-br/library/windows/desktop/ms633576
+ * criando uma WindowClass - https://msdn.microsoft.com/pt-br/library/windows/desktop/ms633576
  */
 public class Main {
 	/*
@@ -19,4 +19,8 @@ public class Main {
 	 * 3. Create a window using the created window class
 	 * 4. Use the WinProc to handle shutdown events - https://stackoverflow.com/questions/40825747/close-my-program-before-windows-shutdown
 	 */
+
+	public static void main(String[] args) {
+
+	}
 }
