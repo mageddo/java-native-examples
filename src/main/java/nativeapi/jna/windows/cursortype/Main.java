5a4bfcf2a133b62d9ee86bb689e5f933ad0255c0
mageddo <edigitalb@gmail.com>
2017-12-19 01:58:01 -0200

    map by referemce

--- src/main/java/nativeapi/jna/windows/cursortype/Main.java
+++ src/main/java/nativeapi/jna/windows/cursortype/Main.java
@@ -11,15 +11,12 @@ import java.util.*;
 
 /**
  * https://msdn.microsoft.com/pt-br/library/windows/desktop/ms648029(v=vs.85).aspx
- * https://github.com/adobe/chromium/blob/master/webkit/glue/webcursor_win.cc
- * https://github.com/java-native-access/jna/blob/master/contrib/platform/test/com/sun/jna/platform/win32/GDI32Test.java
- * https://github.com/tpn/winsdk-10/blob/master/Include/10.0.10240.0/um/WinUser.h
- * https://stackoverflow.com/questions/3610565/why-does-makeintresource-work
- * https://msdn.microsoft.com/en-us/library/windows/desktop/ms681386(v=vs.85).aspx
- * https://msdn.microsoft.com/en-us/library/windows/desktop/ms648045(v=vs.85).aspx
- * https://msdn.microsoft.com/en-us/library/windows/desktop/ms648051(v=vs.85).aspx
+ * Chromium cursor map -  https://github.com/mageddo/chromium/blob/master/webkit/glue/webcursor_win.cc
+ * Load icon example - https://github.com/java-native-access/jna/blob/master/contrib/platform/test/com/sun/jna/platform/win32/GDI32Test.java#L54
+ * understanding makeintresource - https://stackoverflow.com/questions/3610565/why-does-makeintresource-work
+ * all possible windows error codes - https://msdn.microsoft.com/en-us/library/windows/desktop/ms681386(v=vs.85).aspx
+ * Cursor ids - https://msdn.microsoft.com/en-us/library/windows/desktop/ms648391(v=vs.85).aspx
  *
- * @deprecated - it is incomplete, this sample is not working yet, see src/main/c/cmd/windows/cursortype.c
  */
 public class Main {
 
@@ -31,35 +28,6 @@ public class Main {
 		cursors = loadCursors();
 	}
 
-	public enum Cursor {
-		APPSTARTING(32650),
-		NORMAL(32512),
-		CROSS(32515),
-		HAND(32649),
-		HELP(32651),
-		IBEAM(32513),
-		NO(32648),
-		SIZEALL(32646),
-		SIZENESW(32643),
-		SIZENS(32645),
-		SIZENWSE(32642),
-		SIZEWE(32644),
-		UP(32516),
-		WAIT(32514),
-		PEN(32631),
-		SIZE(1029)
-		;
-
-		private final int code;
-		Cursor(final int code) {
-			this.code = code;
-		}
-
-		public int getCode() {
-			return code;
-		}
-	}
-
 	/**
 	 * Load all possible cursors to a map
 	 */
@@ -67,45 +35,25 @@ public class Main {
 		final Map<WinNT.HANDLE, Cursor> cursors = new HashMap<>();
 		for (final Cursor cursor : Cursor.values()) {
 
-			final Memory memory = new Memory(Native.getNativeSize(Long.class, null));
-			memory.setLong(0, cursor.getCode());
-			final Pointer resource = memory.getPointer(0);
-
-			final WinNT.HANDLE hcursor = User32.INSTANCE.LoadImageA(
-				null, resource, WinUser.IMAGE_CURSOR, 0, 0, WinUser.LR_SHARED
-			);
-			if(hcursor == null || Native.getLastError() != 0){
-				throw new Error("Cursor could not be loaded: " + String.valueOf(Native.getLastError()));
+			final WinNT.HANDLE hcursor;
+			if(cursor.getCode() != -1){
+				final Memory memory = new Memory(Native.getNativeSize(Long.class, null));
+				memory.setLong(0, cursor.getCode());
+				final Pointer resource = memory.getPointer(0);
+				hcursor = User32.INSTANCE.LoadImageA(
+					null, resource, WinUser.IMAGE_CURSOR, 0, 0, WinUser.LR_SHARED
+				);
+				if(hcursor == null || Native.getLastError() != 0){
+					throw new Error("Cursor could not be loaded: " + String.valueOf(Native.getLastError()));
+				}
+//				System.out.printf("hcursor=%s, enum=%s\n", hcursor, cursor);
+			} else {
+				hcursor = new WinDef.HCURSOR(new Pointer(cursor.getAddress()));
 			}
+
 			cursors.put(hcursor, cursor);
 		}
 		return Collections.unmodifiableMap(cursors);
-	}
-
-	public interface User32 extends com.sun.jna.Library {
-		User32 INSTANCE = Native.loadLibrary("User32.dll", User32.class);
-
-//		BOOL WINAPI GetCursorInfo(
-//			_Inout_ PCURSORINFO pci
-//		);
-		int GetCursorInfo(CURSORINFO cursorinfo);
-
-//		HANDLE WINAPI LoadImage(
-//			_In_opt_ HINSTANCE hinst,
-//			_In_     LPCTSTR   lpszName,
-//			_In_     UINT      uType,
-//			_In_     int       cxDesired,
-//			_In_     int       cyDesired,
-//			_In_     UINT      fuLoad
-//		);
-		WinNT.HANDLE LoadImageA(
-			WinDef.HINSTANCE hinst,
-			Pointer lpszName,
-			int uType,
-			int cxDesired,
-			int cyDesired,
-			int fuLoad
-		);
 	}
 
 	public static void main(String[] args) throws InterruptedException, IOException {
@@ -122,6 +70,8 @@ public class Main {
 		if(success != 1){
 			throw new Error("Could not retrieve cursor info: " + String.valueOf(Native.getLastError()));
 		}
+
+		// you can use the address printed here to map the others cursors like ALL_SCROLL
 		System.out.printf("currentPointer=%s%n", cursorinfo.hCursor.getPointer());
 		if(cursors.containsKey(cursorinfo.hCursor)){
 			return cursors.get(cursorinfo.hCursor);
@@ -148,6 +98,75 @@ public class Main {
 		@Override
 		protected List<String> getFieldOrder() {
 			return Arrays.asList("cbSize", "flags", "hCursor", "ptScreenPos");
+		}
+	}
+
+	public interface User32 extends com.sun.jna.Library {
+		User32 INSTANCE = Native.loadLibrary("User32.dll", User32.class);
+
+		//		BOOL WINAPI GetCursorInfo(
+//			_Inout_ PCURSORINFO pci
+//		);
+		int GetCursorInfo(CURSORINFO cursorinfo);
+
+		//		HANDLE WINAPI LoadImage(
+//			_In_opt_ HINSTANCE hinst,
+//			_In_     LPCTSTR   lpszName,
+//			_In_     UINT      uType,
+//			_In_     int       cxDesired,
+//			_In_     int       cyDesired,
+//			_In_     UINT      fuLoad
+//		);
+		WinNT.HANDLE LoadImageA(
+			WinDef.HINSTANCE hinst,
+			Pointer lpszName,
+			int uType,
+			int cxDesired,
+			int cyDesired,
+			int fuLoad
+		);
+	}
+
+	public enum Cursor {
+		APPSTARTING(32650),
+		NORMAL(32512),
+		CROSS(32515),
+		HAND(32649),
+		HELP(32651),
+		IBEAM(32513),
+		NO(32648),
+		SIZEALL(32646),
+		SIZENESW(32643),
+		SIZENS(32645),
+		SIZENWSE(32642),
+		SIZEWE(32644),
+		UP(32516),
+		WAIT(32514),
+		PEN(32631),
+
+		// UNMAPPED CURSORS (identified by address, manual test here to discover the address)
+		ALL_SCROLL(-1, 0xff10323),
+		ZOOM_IN(-1, 0xa580327)
+		;
+
+		private final int code;
+		private final int address;
+
+		Cursor(final int code) {
+			this.code = code;
+			this.address = -1;
+		}
+		Cursor(final int code, final int address) {
+			this.code = code;
+			this.address = address;
+		}
+
+		public int getCode() {
+			return code;
+		}
+
+		public int getAddress() {
+			return address;
 		}
 	}
 }
